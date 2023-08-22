979826bacf8e2b631ba823ce121e576c3c65bc61
Giri536 <142829093+Giri536@users.noreply.github.com>
2023-08-22 13:50:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone Giri536/giribots /giribots
+  git clone https://github.com/Giri536/Vjclonerepo /giribots
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /giribots

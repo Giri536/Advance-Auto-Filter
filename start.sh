becbf2ed3d703d93b5da3f351fd8ec7c1ef4aad6
Giri536 <142829093+Giri536@users.noreply.github.com>
2023-08-22 10:28:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone Giri536/Vjclonerepo/Vjclonerepo
+  git clone Giri536/giribots/giribots
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Vjclonerepo
+  git clone $UPSTREAM_REPO /giribots
 fi
-cd /Vjclonerepo
+cd /giribots
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
